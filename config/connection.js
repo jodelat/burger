e0fabd01a6a7d1021fe15f7f8e3728b4f1a66a99
Josiah De La Torre <jodelat@yahoo.com>
2017-09-05 19:49:45 -0700

    burger handlebars hw

---
+++ config/connection.js
@@ -0,0 +1,17 @@
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+  port: 3306,
+host: "localhost",
+user: "root",
+password: "program1",
+database: "burgers_db"
+});
+
+connection.connect(function(err){
+  if (err) throw err;
+
+  console.log("connected as id " + connection.threadId);
+});
+
+module.exports = connection;
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ("Classic");
+INSERT INTO burgers (burger_name) VALUES ("Avacado");
+INSERT INTO burgers (burger_name) VALUES ("Animal Style");
